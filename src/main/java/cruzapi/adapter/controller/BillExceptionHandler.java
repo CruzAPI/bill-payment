a7adc5c7e0fba0b1c277315ef8d3f9196746b007
CruzAPI <guilhermecruz1574@gmail.com>
2022-10-18 22:39:31 -0300

    Handle MethodArgumentNotValidException

--- src/main/java/cruzapi/adapter/controller/BillExceptionHandler.java
+++ src/main/java/cruzapi/adapter/controller/BillExceptionHandler.java
@@ -1,6 +1,8 @@
 package cruzapi.adapter.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.client.HttpStatusCodeException;
@@ -11,14 +13,27 @@ import cruzapi.core.exception.BillException;
 public class BillExceptionHandler
 {
 	@ExceptionHandler(BillException.class)
-	public ResponseEntity<?> handleBillException(BillException e)
+	public ResponseEntity<String> handle(BillException e)
 	{
 		return ResponseEntity.badRequest().body(e.getMessage());
 	}
 	
 	@ExceptionHandler(HttpStatusCodeException.class)
-	public ResponseEntity<?> handleHttpStatusCodeException(HttpStatusCodeException e)
+	public ResponseEntity<String> handle(HttpStatusCodeException e)
 	{
 		return ResponseEntity.status(e.getStatusCode()).body(e.getResponseBodyAsString());
+	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<String> handle(MethodArgumentNotValidException e)
+	{
+		FieldError fieldError = e.getFieldError();
+		
+		if(fieldError != null)
+		{
+			return ResponseEntity.badRequest().body(fieldError.getDefaultMessage());
+		}
+		
+		return ResponseEntity.badRequest().build();
 	}
 }
--- src/main/java/cruzapi/adapter/dto/BillDTO.java
+++ src/main/java/cruzapi/adapter/dto/BillDTO.java
@@ -3,7 +3,6 @@ package cruzapi.adapter.dto;
 import java.time.LocalDate;
 
 import javax.validation.constraints.FutureOrPresent;
-import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
@@ -16,11 +15,11 @@ import lombok.Data;
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class BillDTO
 {
-	@Pattern(regexp = "[0-9]{44}")
-	@NotBlank
+	@Pattern(regexp = "[0-9]{44}", message = "Invalid bar code.")
+	@NotNull(message = "Bar code must not be null.")
 	private String barCode;
 	
-	@FutureOrPresent
-	@NotNull
+	@FutureOrPresent(message = "Payment date must be a date in the present or in the future.")
+	@NotNull(message = "Payment date must not be null.")
 	private LocalDate paymentDate;
 }
